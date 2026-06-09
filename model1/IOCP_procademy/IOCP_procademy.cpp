6a5f666b299e7094ff93ce294d771f5ae95fe447
mumyung3 <daehwan11114@gmail.com>
2026-06-10 05:45:59 +0900

    iocount 수정

--- model1/IOCP_procademy/IOCP_procademy.cpp
+++ model1/IOCP_procademy/IOCP_procademy.cpp
@@ -24,14 +24,14 @@ struct MYOVERLAPPED {
 };
 
 struct SOCKETINFO {
-	__int64 sessionID;
-	SOCKET sock;
-	MYOVERLAPPED overlapped;
-	WSABUF RecvWSABuf;
-	WSABUF SendWSABuf;
-	RingBuffer RecvQ;
-	RingBuffer SendQ;
-	SOCKADDR_IN clientAddr;  // 추가
+	__int64 sessionID{};
+	SOCKET sock{};
+	MYOVERLAPPED overlapped{};
+	WSABUF RecvWSABuf{};
+	WSABUF SendWSABuf{};
+	RingBuffer RecvQ{};
+	RingBuffer SendQ{};
+	SOCKADDR_IN clientAddr{};  // 추가
 	LONG ioCount = 0;
 	bool disconnected = false;
 };
@@ -58,9 +58,9 @@ int main()
 {
 	_wsetlocale(LC_ALL, L"");  // 시스템 로케일 사용
 
-	//이벤트 생성 세션 전부 제거 대기용
+	//이벤트 생성. 세션 전부 제거 대기용
 	hSessionEmpty = CreateEvent(NULL, TRUE, FALSE, NULL);
-
+	if (hSessionEmpty == NULL) return 1;
 
 	int retval;
 
@@ -74,13 +74,18 @@ int main()
 	if (hcp == NULL) return 1;
 
 	// 스레드 생성 시 핸들 보관
-	HANDLE hThreads[64]{};
+	HANDLE hThreads[64] = {};
 
 	// 논리프로세서 * n 개 스레드 생성
-	HANDLE hThread;
-	for (int i = 0; i < (int)si.dwNumberOfProcessors * 2; i++) {
+	HANDLE hThread{};
+	int threadCount = ((int)si.dwNumberOfProcessors - MINUSCONCURRENTTHREADS) * 2;
+	for (int i = 0; i < threadCount; i++) {
 		hThreads[i] = CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
-		if (hThreads[i] == NULL) return 1;
+		if (hThreads[i] == NULL) {
+			for (int j = 0; j < i; j++)
+				CloseHandle(hThreads[j]);
+			return 1;
+		}
 	}
 
 	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -100,7 +105,7 @@ int main()
 
 	ACCEPTINFO acceptInfo = { listen_sock, hcp };
 	HANDLE hAcceptThread = CreateThread(NULL, 0, AcceptThread, &acceptInfo, 0, NULL);
-	int threadCount;
+	if (hAcceptThread == NULL) return 1;
 
 	// main 루프 - 키보드 서버 제어
 	while (1) {
@@ -122,7 +127,6 @@ int main()
 
 				WaitForSingleObject(hSessionEmpty, INFINITE);  // 항상 대기
 
-				threadCount = (int)si.dwNumberOfProcessors * 2;
 				for (int i = 0; i < threadCount; i++)
 					PostQueuedCompletionStatus(hcp, 0, (ULONG_PTR)-1, NULL);
 
@@ -134,7 +138,12 @@ int main()
 
 	WaitForMultipleObjects(threadCount, hThreads, TRUE, INFINITE);
 	for (int i = 0; i < threadCount; i++)
-		CloseHandle(hThreads[i]);
+	{
+		if (hThreads[i] != NULL)
+#pragma warning(suppress: 6001)
+			CloseHandle(hThreads[i]);
+
+	}
 
 	WaitForSingleObject(hAcceptThread, INFINITE);  // ← 추가
 	CloseHandle(hAcceptThread);                     // ← 추가
@@ -153,20 +162,19 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 
 		DWORD cbTransferred = 0;
 		ULONG_PTR completionKey = 0;
-		SOCKETINFO* session;
-		LPOVERLAPPED pov;
+		SOCKETINFO* session{};
+		LPOVERLAPPED pov{};
 		retval = GetQueuedCompletionStatus(hcp, &cbTransferred, (PULONG_PTR)&completionKey, (LPOVERLAPPED*)&pov, INFINITE);
-		session = (SOCKETINFO*)completionKey;  // 여기서 캐스팅
 
 		// 종료 신호
 		if (completionKey == (ULONG_PTR)-1) {
 			return 0;
 		}
+		session = (SOCKETINFO*)completionKey;  // 여기서 캐스팅
 
 		// 오류 처리 timeout / iocp 자체 실패(이건 고려 x) 
-		if (pov == nullptr) continue;
+		if (retval == 0 && pov == nullptr) continue;
 
-		LONG remaining = InterlockedDecrement(&session->ioCount);  // ← 추가
 
 
 		// 비동기 입출력 결과확인
@@ -174,7 +182,7 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 			session->disconnected = true;
 
 
-
+			// 리턴 false에 pov 널이 아닐때, (io 실패) -> transfer = 0, key = session 포인터 pov = 해당 overlap 설정됨.
 			if (retval == 0) {
 				// overlap null이면서 반환값 실패 (io 실패) 연결끊김
 				DWORD temp1, temp2;
@@ -188,6 +196,7 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 			wprintf(L"[TCP 서버] 클라이언트 종료: IP 주소 = %S, 포트 번호=%d\n",
 				inet_ntoa(session->clientAddr.sin_addr), ntohs(session->clientAddr.sin_port));
 
+			LONG remaining = InterlockedDecrement(&session->ioCount);  // 종료시 io count 감소
 			if (remaining == 0) {
 
 				closesocket(session->sock); // 서버 종료시 2번 해제하는거같은데, 실제로 실패반환이면 문제 없는듯?
@@ -204,10 +213,13 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 			continue;
 		}
 
+		// 리턴값 성공!
 		if (pov == &session->overlapped.recvOverlapped) {
 			session->RecvQ.MoveRear(cbTransferred);
 			// 받은 데이터 출력
-			wchar_t tempBuf[BUFSIZE + 1];
+			// 스택이 너무 커짐. 더미에서 출력을 확인하던 디버깅으로 확인하자!
+			/*/
+			wchar_t tempBuf[BUFSIZE + 1]{};
 			int useSize = session->RecvQ.GetUseSize();
 			session->RecvQ.Peek((char*)tempBuf, useSize);
 			tempBuf[useSize / sizeof(wchar_t)] = L'\0';
@@ -215,21 +227,45 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 				inet_ntoa(session->clientAddr.sin_addr),
 				ntohs(session->clientAddr.sin_port),
 				tempBuf);
+				//*/
 
-			// 에코: recvQ → sendQ 복사
-			session->RecvQ.Dequeue((char*)tempBuf, useSize);
-			session->SendQ.Enqueue((char*)tempBuf, useSize);
+				// 에코: recvQ → sendQ 복사
+			int useSize = session->RecvQ.GetUseSize();
+			if (session->SendQ.GetFreeSize() >= useSize) // 버퍼가 부족해서 로직 못돌아가니 사실상 여기가 연결끊기 해야함. 데이터가 안간다면 이쪽부분 따로 로직 나중에 추가하자!
+			{
+				session->RecvQ.Dequeue(session->SendQ.GetRearBufferPtr(), useSize);
+				session->SendQ.MoveRear(useSize);
+			}
+			else // 연결끊기!
+			{
+				session->disconnected = true;
+				LONG remaining = InterlockedDecrement(&session->ioCount);
+				if (remaining == 0) {
+					closesocket(session->sock);
+					AcquireSRWLockExclusive(&sessionLock);
+					sessionMap.erase(session->sessionID);
+					bool empty = sessionMap.empty();
+					ReleaseSRWLockExclusive(&sessionLock);
+					delete session;
+					if (empty) SetEvent(hSessionEmpty);
+				}
+				continue;
+			}
 
 			// WSASend 등록
-			session->SendWSABuf.buf = session->SendQ.GetFrontBufferPtr();
-			session->SendWSABuf.len = session->SendQ.DirectDequeueSize();
-			PostSend(session);
+			if (session->SendQ.GetUseSize() > 0) {
+				session->SendWSABuf.buf = session->SendQ.GetFrontBufferPtr();
+				session->SendWSABuf.len = session->SendQ.DirectDequeueSize();
+				PostSend(session);
+			}
 
 			// recv 즉시 재등록
 			session->RecvWSABuf.buf = session->RecvQ.GetRearBufferPtr();
 			session->RecvWSABuf.len = session->RecvQ.DirectEnqueueSize();
 			DWORD flags = 0;
 			PostRecv(session);
+
+
 		}
 		else {
 			// send 완료 → sendQ에 남은 데이터 있으면 다시 send
@@ -244,6 +280,7 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 
 		}
 
+		LONG remaining = InterlockedDecrement(&session->ioCount);  // recv/send 완료시 io count 감소
 
 		if (session->disconnected && remaining == 0) {
 			closesocket(session->sock);
@@ -264,9 +301,9 @@ DWORD WINAPI WorkerThread(LPVOID arg) {
 DWORD WINAPI AcceptThread(LPVOID arg) {
 	ACCEPTINFO* info = (ACCEPTINFO*)arg;
 
-	SOCKADDR_IN clientaddr;
+	SOCKADDR_IN clientaddr{};
 	int addrlen;
-	DWORD recvbytes, flags;
+	DWORD recvbytes{}, flags{};
 
 	while (1) {
 		addrlen = sizeof(clientaddr);
